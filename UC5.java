c738aac2491ef7854cc50a5ec09d45bd885fa89c
vb7842-rgb <vb7842@srmist.edu.in>
2026-03-17 09:18:31 +0530

    Add files via upload

---
+++ UC5.java
@@ -0,0 +1,13 @@
+public class HelloEnhanced {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
+        }
+
+    }
+}
