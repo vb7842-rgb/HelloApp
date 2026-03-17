5f8c910b02b2ed437a28eda76aa469b9291be070
vb7842-rgb <vb7842@srmist.edu.in>
2026-03-17 09:17:20 +0530

    Add files via upload

---
+++ UC4.java
@@ -0,0 +1,13 @@
+public class HelloMultiple {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
+        }
+
+    }
+}
